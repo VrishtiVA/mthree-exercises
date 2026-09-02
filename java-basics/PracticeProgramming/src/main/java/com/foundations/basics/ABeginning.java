c041b2f710cd00aaec93f169518af29f2724d4d1
VrishtiVA <vaprojectcs@gmail.com>
2026-09-02 23:44:29 +0100

    Completed Practice Programming Basics

---
+++ java-basics/PracticeProgramming/src/main/java/com/foundations/basics/ABeginning.java
@@ -0,0 +1,9 @@
+package com.foundations.basics;
+
+public class ABeginning {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello from mthree!");
+        System.out.println("Typing code is easier that you think!");
+    }
+}
