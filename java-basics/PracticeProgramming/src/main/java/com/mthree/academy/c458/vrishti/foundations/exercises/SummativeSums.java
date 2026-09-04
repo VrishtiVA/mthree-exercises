e9593ce7bff6b89dd4128994422633dc9b69ef9c
VrishtiVA <vaprojectcs@gmail.com>
2026-09-04 13:37:22 +0100

    Completed Summative Sums Practice Exercise

---
+++ java-basics/PracticeProgramming/src/main/java/com/mthree/academy/c458/vrishti/foundations/exercises/SummativeSums.java
@@ -0,0 +1,24 @@
+package com.mthree.academy.c458.vrishti.foundations.exercises;
+
+public class SummativeSums {
+
+    public static void main(String[] args) {
+
+        System.out.println("#1 Array Sum: " + sumArray(new int[]{1, 90, -33, -55, 67, -16, 28, -55, 15}));
+        System.out.println("#2 Array Sum: " + sumArray(new int[]{999, -60, -77, 14, 160, 301}));
+        System.out.println("#3 Array Sum: " + sumArray(new int[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, -99}));
+
+    }
+
+    public static int sumArray(int[] arr) {
+        
+        int total = 0;
+
+        for (int num : arr) {
+            total += num;
+        }
+
+        return total;
+    }
+
+}
