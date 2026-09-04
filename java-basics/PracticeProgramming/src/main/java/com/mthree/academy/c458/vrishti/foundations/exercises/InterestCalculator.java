c81cac4eef1232bf13f869f368374c3fd722c39d
VrishtiVA <vaprojectcs@gmail.com>
2026-09-04 12:51:25 +0100

    Implemented Additional Challenges in Salary Calculator

--- java-basics/PracticeProgramming/src/main/java/com/mthree/academy/c458/vrishti/foundations/exercises/InterestCalculator.java
+++ java-basics/PracticeProgramming/src/main/java/com/mthree/academy/c458/vrishti/foundations/exercises/InterestCalculator.java
@@ -17,24 +17,57 @@ import java.util.Scanner;
  */
 public class InterestCalculator {
 
+    final static Scanner INPUT_READER = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         //Desired inputs
         float annualInterestRate;
         float principal;
         float yearsToStayInFund;
+        float mode;
 
         //Utility variables
-        float quarterlyInterestRate;
+        float brokenInterestRate;
         float previousPrincipal;
 
         //Take inputs
         principal = takeValidFloatInput("How much money do you want to invest?");
         yearsToStayInFund = takeValidFloatInput("How many years are you investing?");
         annualInterestRate = takeValidFloatInput("What is the annual interest rate % growth?");
+        INPUT_READER.nextLine(); //Absorb line.
+
+        //Take mode input
+        do {
+            System.out.print("Do you want to apply your interest quarterly, monthly, or daily? ");
+            try {
+                switch (INPUT_READER.nextLine().substring(0, 1).toUpperCase()) {
+                    case "Q":
+                        System.out.println("Applying interest Quarterly");
+                        mode = 4f;
+                        break;
+                    case "M":
+                        System.out.println("Applying interest Monthly");
+                        mode = 12f;
+                        break;
+                    case "D":
+                        System.out.println("Applying interest Daily");
+                        mode = 365.25f;
+                        break;
+                    default:
+                        System.out.println("Applying interest Annually");
+                        mode = 1f;
+                        break;
+                }
+                break;
+            } catch (Exception ex) {
+                System.out.println("Invalid input. Please try again");
+            }
+        } while (true);
+        System.out.println();
 
         //Calculate quarterly interest rate
-        quarterlyInterestRate = annualInterestRate / 4f;
+        brokenInterestRate = annualInterestRate / mode;
 
         //Calculations per year
         System.out.println("Calculating...");
@@ -42,7 +75,7 @@ public class InterestCalculator {
 
             //Calculate principal at end, and earnings
             previousPrincipal = principal;
-            principal = (float) (principal * Math.pow(1 + (quarterlyInterestRate / 100), 4));
+            principal = (float) (principal * Math.pow(1 + (brokenInterestRate / 100), 4));
 
             //Output year summary
             System.out.printf("Year %d:\n", i);
@@ -60,8 +93,6 @@ public class InterestCalculator {
      */
     private static float takeValidFloatInput(String question) {
 
-        Scanner inputReader = new Scanner(System.in);
-
         //Desired input
         float number;
 
@@ -69,14 +100,14 @@ public class InterestCalculator {
         do {
             System.out.print(question + " ");
             try {
-                number = inputReader.nextFloat();
+                number = INPUT_READER.nextFloat();
                 return number; //Return valid input.
 
             } catch (NumberFormatException e) {
                 System.out.println("Invalid input. Please try again");
             }
 
-        } while(true);
+        } while (true);
 
     }
 }
