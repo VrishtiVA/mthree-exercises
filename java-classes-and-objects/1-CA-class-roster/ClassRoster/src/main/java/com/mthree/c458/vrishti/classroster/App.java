6c58bee2bec41548b64950ade3bc3380ed586ef6
VrishtiVA <vaprojectcs@gmail.com>
2026-09-10 23:30:56 +0100

    Class Roster CA: Added menu to Controller
    This should be refactored out

--- java-classes-and-objects/1-CA-class-roster/ClassRoster/src/main/java/com/mthree/c458/vrishti/classroster/App.java
+++ java-classes-and-objects/1-CA-class-roster/ClassRoster/src/main/java/com/mthree/c458/vrishti/classroster/App.java
@@ -1,4 +1,14 @@
 package com.mthree.c458.vrishti.classroster;
 
+import com.mthree.c458.vrishti.classroster.controller.ClassRosterController;
+
 public class App {
+
+    public static void main(String[] args) {
+
+        //Use controller to call the run method.
+        ClassRosterController controller = new ClassRosterController();
+        controller.run();
+
+    }
 }
--- java-classes-and-objects/1-CA-class-roster/ClassRoster/src/main/java/com/mthree/c458/vrishti/classroster/controller/ClassRosterController.java
+++ java-classes-and-objects/1-CA-class-roster/ClassRoster/src/main/java/com/mthree/c458/vrishti/classroster/controller/ClassRosterController.java
@@ -1,7 +1,60 @@
 package com.mthree.c458.vrishti.classroster.controller;
 
+import com.mthree.c458.vrishti.classroster.ui.UserIO;
+import com.mthree.c458.vrishti.classroster.ui.UserIOConsoleImpl;
+
 /**
  * This is the orchestrator of the application. It knows what needs to be done, when it needs to be done, and what component can do the job.
  */
 public class ClassRosterController {
+
+    public UserIO io = new UserIOConsoleImpl();
+
+    public void run() {
+
+        boolean keepGoing = true;
+        int menuSelection = 0;
+
+        do {
+
+            //Show the menu
+            io.print(
+                "Main Menu" +
+                "\n1. List Student IDs" +
+                "\n2. Create New Student" +
+                "\n3. View a Student" +
+                "\n4. Remove a Student" +
+                "\n5. Exit"
+            );
+
+            //Allow user selection
+            menuSelection = io.readInt("Please select from the above choices : ", 1, 5);
+
+            //Action user choice
+            switch (menuSelection) {
+                case 1:
+                    io.print("LIST STUDENTS");
+                    break;
+                case 2:
+                    io.print("CREATE STUDENT");
+                    break;
+                case 3:
+                    io.print("VIEW STUDENT");
+                    break;
+                case 4:
+                    io.print("REMOVE STUDENT");
+                    break;
+                case 5:
+                    keepGoing = false;
+                    break;
+                default:
+                    io.print("UNKNOWN COMMAND");
+            }
+
+        } while (keepGoing);
+
+        io.print("GOOD BYE");
+
+    }
+
 }
