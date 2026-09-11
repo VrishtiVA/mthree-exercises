992a22b25c0da58d579e58f1703fc35d15e69388
VrishtiVA <vaprojectcs@gmail.com>
2026-09-11 18:39:19 +0100

    Created empty test class

---
+++ java-classes-and-objects/1-CA-class-roster/ClassRoster/src/test/java/com/mthree/c458/vrishti/classroster/dao/ClassRosterDaoFileImplTest.java
@@ -0,0 +1,29 @@
+package com.mthree.c458.vrishti.classroster.dao;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ClassRosterDaoFileImplTest {
+
+    public ClassRosterDaoFileImplTest() {}
+
+    @BeforeAll
+    public static void setUpClass() {}
+
+    @AfterAll
+    public static void tearDownClass() {}
+
+    @BeforeEach
+    public void setUp() {}
+
+    @AfterEach
+    public void tearDown() {}
+
+    @Test
+    public void testSomeMethod() {
+        fail("This test case is a prototype");
+    }
+
+}
