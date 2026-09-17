776748b18d8895dca3414941bff750cbc468716b
VrishtiVA <vaprojectcs@gmail.com>
2026-09-18 00:22:28 +0100

    Converting DVD Library to use Spring DI

--- intermediate-java/PracticeProgramming/src/main/java/mthree/academy/c458/vrishti/dvd_library/App.java
+++ intermediate-java/PracticeProgramming/src/main/java/mthree/academy/c458/vrishti/dvd_library/App.java
@@ -7,16 +7,21 @@ import mthree.academy.c458.vrishti.dvd_library.service.DVDLibraryService;
 import mthree.academy.c458.vrishti.dvd_library.service.DVDLibraryServiceImpl;
 import mthree.academy.c458.vrishti.dvd_library.ui.DVDLibraryView;
 import mthree.academy.c458.vrishti.dvd_library.ui.UserIOConsoleImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
 
-        //Instantiate components
-        DVDLibraryView view = new DVDLibraryView(new UserIOConsoleImpl());
-        DVDLibraryDao dao = new DVDLibraryDaoFileImpl("dvd-library.txt");
-        DVDLibraryService service = new DVDLibraryServiceImpl(dao);
-        DVDLibraryController controller = new DVDLibraryController(view, service);
+//        //Instantiate components
+//        DVDLibraryView view = new DVDLibraryView(new UserIOConsoleImpl());
+//        DVDLibraryDao dao = new DVDLibraryDaoFileImpl("dvd-library.txt");
+//        DVDLibraryService service = new DVDLibraryServiceImpl(dao);
+//        DVDLibraryController controller = new DVDLibraryController(view, service);
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        DVDLibraryController controller = applicationContext.getBean("controller", DVDLibraryController.class);
 
         //Call run method from controller to start application
         controller.run();
